174a14b056a8a91e9078f0b31017d509fd2dadd9
Jimmy <cwu023@gmail.com>
2018-09-16 02:14:33 -0700

    Update landing page.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,24 @@ $(function() {
   });
 });
 
+//Scroll Top Animation
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $("#topArrow").fadeIn();
+  } else {
+    $("#topArrow").fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#topArrow").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+});
+
+//Full page scroll
 $(function() {
   $.scrollify({
     section: ".pg",
