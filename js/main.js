bab1ae5bddd240492dbdf2b4cf1609a26b0ca25f
Jimmy <cwu023@gmail.com>
2018-11-04 19:21:57 -0800

    Redesign & struce the website
    Remove landing page scrolling and various changes.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// Scroll Down Animation
-$(function() {
-  $(".scroll-down").click(function() {
-    $("html, body").animate(
-      { scrollTop: $("#section02").offset().top },
-      "slow"
-    );
-    return false;
-  });
-});
-
 //Scroll Top Animation
 $(window).scroll(function() {
   var height = $(window).scrollTop();
@@ -23,15 +12,5 @@ $(document).ready(function() {
     event.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
-  });
-});
-
-//Full page scroll
-$(function() {
-  $.scrollify({
-    section: ".pg",
-    scrollSpeed: 600,
-    setHeights: false,
-    easing: "linear"
   });
 });
